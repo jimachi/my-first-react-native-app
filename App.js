a8872d553588196106ffc3facf3ce28e32f616f4
Yuichiro Iijima <jimaichiro@gmail.com>
2022-04-14 01:55:42 +0900

    feat: Welcomeコンポーネントの実装

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Welcome } from './components/Welcome';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <StatusBar style="auto" />
+      <Welcome name="ねこねこ" />
     </View>
   );
 }
---
+++ components/Welcome.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+export function Welcome(props) {
+  return (
+    <Text>にゃおーん, {props.name}</Text>
+  );
+};
